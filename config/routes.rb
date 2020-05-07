9ed88aa1339c3266c06dd54a429603be95a3e259
Alisa Chang <laptite@gmail.com>
2020-05-06 19:47:29 -0700

    Add additional recipe validation, api controller and upadte routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:name', to: 'recipes#show'
+      delete '/destroy/:name', to: 'recipes#destroy'
+    end
+  end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
 end
