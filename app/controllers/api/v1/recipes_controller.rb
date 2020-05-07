6d4d7594d185c9abc2037116ab2c0c2f735fe033
Alisa Chang <laptite@gmail.com>
2020-05-06 20:36:54 -0700

    Add Recipes components class

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,13 +1,40 @@
 class Api::V1::RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :destroy]
+
   def index
+    @recipe = Recipe.all.order(created_at: :desc)
+    render json: @recipe
   end
 
   def create
+    @recipe = Recipe.create!(recipe_params)
+    if @recipe
+      render json: @recipe
+    else
+      render json: @recipe.errors
+    end
   end
 
   def show
+    if @recipe
+      render json: @recipe
+    else
+      render json: @recipe.errors
+    end
   end
 
   def destroy
+    @recipe&.destroy
+    render json: { message: 'Recipe has been deleted'}
   end
+
+  private
+
+    def recipe_params
+      params.permit(:name, :image, :ingredients, :directions, :notes)
+    end
+
+    def find_recipe
+      @recipe = Recipe.find(params[:id])
+    end
 end
